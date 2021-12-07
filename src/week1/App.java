75a84dc885f08b71f8366947f4737cd70be167d1
Octal1 <vonthoth@gamil.com>
2021-12-06 20:30:36 -0700

    intial commit

---
+++ src/week1/App.java
@@ -0,0 +1,21 @@
+package week1;
+
+public class App {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Testing.");
+		
+		
+		
+		String firstName = "Tom";
+		
+		String lastName = "Sawyer";
+		
+		System.out.println(firstName + "lastName");
+		
+		
+		//System.out.println(answer);
+	
+	}
+
+}
---
+++ src/week1/lecture/Lecture.java
@@ -0,0 +1,5 @@
+package week1.lecture;
+
+public class Lecture {
+
+}
